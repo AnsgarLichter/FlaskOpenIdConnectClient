38d4e8890ebb4bcf8c32c37ca70a647d3cde756c
Ansgar Lichter <lichteransgar@gmail.com>
2023-04-13 23:35:21 +0200

    feat(orcid): generalize process, split login and signup

--- views.py
+++ views.py
@@ -60,14 +60,14 @@ def logout():
     return redirect(url_for("views.welcome"))
 
 
-@blueprint.route("/login/google")
+@blueprint.route("/login/google1")
 def login_google():
     redirect_url = url_for("views.authorize_google", _external=True)
 
     return oauth.google.authorize_redirect(redirect_url)
 
 
-@blueprint.route("/login/google/authorize")
+@blueprint.route("/login/google/authorize1")
 def authorize_google():
     token = oauth.google.authorize_access_token()
     print(f"\nToken: {token}\n")
@@ -84,14 +84,14 @@ def authorize_google():
     return redirect(url_for("views.protected"))
 
 
-@blueprint.route("/login/microsoft")
+@blueprint.route("/login/microsoft1")
 def login_microsoft():
     redirect_url = url_for("views.authorize_microsoft", _external=True)
 
     return oauth.microsoft.authorize_redirect(redirect_url)
 
 
-@blueprint.route("/login/microsoft/authorize")
+@blueprint.route("/login/microsoft/authorize1")
 def authorize_microsoft():
     token = oauth.microsoft.authorize_access_token()
     print(f"\nToken: {token}\n")
@@ -108,14 +108,14 @@ def authorize_microsoft():
     return redirect(url_for("views.protected"))
 
 
-@blueprint.route("/login/orcid")
+@blueprint.route("/login/orcid1")
 def login_orcid():
     redirect_url = url_for("views.authorize_orcid", _external=True)
 
     return oauth.orcid.authorize_redirect(redirect_url)
 
 
-@blueprint.route("/login/orcid/authorize")
+@blueprint.route("/login/orcid/authorize1")
 def authorize_orcid():
     token = oauth.orcid.authorize_access_token()
 
@@ -133,14 +133,14 @@ def authorize_orcid():
     return redirect(url_for("views.protected"))
 
 
-@blueprint.route("/login/gitlab")
+@blueprint.route("/login/gitlab1")
 def login_gitlab():
     redirect_url = url_for("views.authorize_gitlab", _external=True)
 
     return oauth.gitlab.authorize_redirect(redirect_url)
 
 
-@blueprint.route("/login/gitlab/authorize")
+@blueprint.route("/login/gitlab/authorize1")
 def authorize_gitlab():
     token = oauth.gitlab.authorize_access_token()
     print(f"\nToken: {token}\n")
@@ -156,30 +156,70 @@ def authorize_gitlab():
     login_user(user)
     return redirect(url_for("views.protected"))
 
-# TODO: Move all login links to login page & only login in called funtions
 # TODO: Add possibilities on signup page to sign up with oidc providers
 # TODO: Add configuration possibilities for needed user parameters
 # TODO: Add possibility to connect multiple services to 1 account?
-@blueprint.route("/login/oidc/<provider>")
+
+
+@blueprint.route("/signup/<provider>")
+def signup_oidc(provider):
+    redirect_url = url_for("views.authorize_signup_oidc",
+                           _external=True, provider=provider)
+    client = oauth.create_client(provider)
+
+    if not client:
+        return
+
+    return client.authorize_redirect(redirect_url)
+
+@blueprint.route("/signup/<provider>/authorize")
+def authorize_signup_oidc(provider):
+    client = oauth.create_client(provider)
+    if not client:
+        return
+
+    token = client.authorize_access_token()
+    print(f"{token}")
+
+    user_info = token['userinfo']
+    user = User.query.filter_by(id=user_info.sub).first()
+    if user:
+        return redirect(url_for("views.login_oidc", provider=provider))
+
+    user_name = user_info.name
+    if provider == 'microsoft':
+        user_name = user_info['preferred_username']
+    elif provider == 'orcid':
+        user_name = f"{user_info['given_name']}{user_info['family_name']}"
+    elif provider == 'gitlab': # user_info.name does work but is already used in the test example
+        user_name = f"{user_name}_gitlab"
+
+    user = User(id=user_info.sub, username=user_name, type=provider)
+    db.session.add(user)
+    db.session.commit()
+
+    login_user(user)
+    return redirect(url_for("views.protected"))
+
+@blueprint.route("/login/<provider>")
 def login_oidc(provider):
-    redirect_url = url_for("views.authorize_oidc",
+    redirect_url = url_for("views.authorize_login_oidc",
                            _external=True, provider=provider)
 
     return oauth.create_client(provider).authorize_redirect(redirect_url)
 
 
-@blueprint.route("/login/oidc/<provider>")
-def authorize_oidc(provider):
-    token = oauth.gitlab.authorize_access_token()
+@blueprint.route("/login/<provider>/authorize")
+def authorize_login_oidc(provider):
+    client = oauth.create_client(provider)
+    token = client.authorize_access_token()
+
     print(f"\nToken: {token}\n")
 
     user_info = token['userinfo']
     user = User.query.filter_by(id=user_info.sub).first()
     if not user:
-        user_name = f"{user_info.given_name}{user_info.family_name}" #TODO: Doesn't work for all providers
-        user = User(id=user_info.sub, username=user_name, type=provider)
-        db.session.add(user)
-        db.session.commit()
+        return redirect(url_for("views.welcome"))
 
     login_user(user)
     return redirect(url_for("views.protected"))
