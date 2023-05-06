b4fa0f47937fb172cbabe293ca4571e63ae982ae
Ansgar Lichter <lichteransgar@gmail.com>
2023-05-06 14:37:47 +0200

    feat: all client properties can be supplioed in the config

--- oauth.py
+++ oauth.py
@@ -4,40 +4,11 @@ from app import app
 
 oauth = OAuth(app)
 
-# TODO: Check if configuration URLs can be moved into config and are still recognized from Authlib
-CONF_URL_GOOGLE = 'https://accounts.google.com/.well-known/openid-configuration'
-CONF_URL_MICROSOFT = 'https://login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad/v2.0/.well-known/openid-configuration'
-CONF_URL_ORCID = 'https://sandbox.orcid.org/.well-known/openid-configuration'
-CONF_URL_GITLAB = 'https://gitlab.com/.well-known/openid-configuration'
 
-oauth.register(
-    name='google',
-    server_metadata_url=CONF_URL_GOOGLE,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+oauth.register(name='google')
 
-oauth.register(
-    name='microsoft',
-    server_metadata_url=CONF_URL_MICROSOFT,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+oauth.register(name='microsoft')
 
-oauth.register(
-    name='orcid',
-    server_metadata_url=CONF_URL_ORCID,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+oauth.register(name='orcid')
 
-oauth.register(
-    name='gitlab',
-    server_metadata_url=CONF_URL_GITLAB,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+oauth.register(name='gitlab')
